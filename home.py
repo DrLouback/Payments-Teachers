a705090bdc88dd550bbf6a60c9df4274baa81e5e
DrLouback <drloubackmateus@gmail.com>
2024-09-11 00:05:21 -0300

    Criando a ingestão de dados para F_Relatorio

--- home.py
+++ home.py
@@ -9,8 +9,9 @@ st.title('Bem vindo ao sistema ')
 st.header('Escolha o Professor')
 lista_professores = pd.read_sql('Select * from Professores',conn)
 
-prof_selecionado = st.selectbox('',lista_professores['name'],label_visibility='hidden')
+prof_selecionado = st.selectbox('Label',lista_professores['name'],label_visibility='hidden')
+id_prof = lista_professores.loc[lista_professores['name']== prof_selecionado, 'id_professor'].values[0] #type: ignore
 
-
-
-
+query = f"Select * from Professores where id_professor = '{id_prof}'"
+df_selecionado = pd.read_sql(query,conn)
+st.dataframe(df_selecionado)
--- pages/datas.py
+++ pages/datas.py
@@ -5,11 +5,36 @@ import sqlite3
 
 db = 'db.sqlite3'
 conn = sqlite3.connect(db)
+professor = F_Relatorio(db)
 
-df_professor = pd.read_sql('select * from Professores', conn)
-professor_names = df_professor['name']
 
-professor = st.selectbox('Nome do professor', professor_names)
-filter_professor = df_professor.loc[df_professor['name'] == professor, 'id_professor'].values[0] #type: ignore
+def selecionar_valor(tabela_pandas, coluna_exibição, coluna_buscada):
+    dataframe = pd.read_sql(f'SELECT * FROM {tabela_pandas}', conn)
+    dado_escolhido = st.selectbox('Escolha', dataframe[coluna_exibição])
+    dado_buscado = dataframe.loc[dataframe[coluna_exibição] == dado_escolhido, coluna_buscada].values[0] #type: ignore
+    return dado_buscado
+col1,col2,col3 = st.columns(3)
+with col1:
+    id_professor = selecionar_valor('Professores','name','id_professor')
+with col2:
+    id_aluno = selecionar_valor('Alunos','name','id')
+with col3:
+    id_contrato = selecionar_valor('Contratos','contrato','id_contrato')
 
-st.header(filter_professor)
+def pegar_valor(tabela_pandas, coluna_ref, coluna_buscada):
+    dataframe = pd.read_sql(f'Select * from {tabela_pandas} ',conn)
+    resultado = dataframe.loc[dataframe[coluna_ref] == coluna_ref, coluna_buscada ].values[0] #type: ignore
+    return resultado
+with col1:
+    data = st.date_input('data')
+if id_contrato:
+    #x_semana = pegar_valor('Contratos',id_contrato,'x_semana')
+    percentual = '35'
+    #valor = pegar_valor('Contratos',id_contrato,'valor')
+    desconto = 0
+    #valor_devido = valor 
+#pandas_teste = st.dataframe([selecionar_id('Professores','name','id_professor'),selecionar_id('Alunos','name','id')])
+
+    frame_teste = pd.DataFrame(columns=['id_professor','id_aluno','id_contrato','x_semana','%','valor','desconto','valor_devido'])
+
+    professor.insert_datas(data, id_professor, id_aluno, id_contrato, x_semana, percentual, valor, desconto, valor_devido)
