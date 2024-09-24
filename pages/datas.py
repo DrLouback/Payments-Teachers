9aecffcaf004c7f9fdd9150b9f17f4a716d4a491
DrLouback <drloubackmateus@gmail.com>
2024-09-24 00:15:59 -0300

    update relatorio

--- pages/datas.py
+++ pages/datas.py
@@ -5,36 +5,56 @@ import sqlite3
 
 db = 'db.sqlite3'
 conn = sqlite3.connect(db)
-professor = F_Relatorio(db)
 
 
-def selecionar_valor(tabela_pandas, coluna_exibição, coluna_buscada):
-    dataframe = pd.read_sql(f'SELECT * FROM {tabela_pandas}', conn)
-    dado_escolhido = st.selectbox('Escolha', dataframe[coluna_exibição])
-    dado_buscado = dataframe.loc[dataframe[coluna_exibição] == dado_escolhido, coluna_buscada].values[0] #type: ignore
-    return dado_buscado
-col1,col2,col3 = st.columns(3)
+#Dataframes tabela professores
+df_professor = pd.read_sql('Select id_professor, name from professores', conn)  
+st.header('Professor')
+professor = st.selectbox('Professores', df_professor['name'], label_visibility='collapsed')
+id_professor = df_professor.loc[df_professor['name'] == professor, 'id_professor'].values[0] #type: ignore
+
+#Dataframes tabelas alunos
+alunos = pd.read_sql('SELECT * FROM Alunos',conn)
+alunos_name = alunos['name']
+
+#Dataframes tabelas contratos
+df_contratos = pd.read_sql('Select * from Contratos', conn)
+contratos_nomes = df_contratos['contrato']
+
+
+
+
+
+#Informações do aluno
+data = st.date_input('Data de matrícula')
+st.subheader('Nome do aluno')
+aluno = st.selectbox('Aluno', alunos_name, label_visibility= 'collapsed')
+id_aluno = alunos.loc[alunos['name'] == aluno, 'id'].values[0] #type: ignore
+contrato_aluno = alunos.loc[alunos['id'] == id_aluno, 'contrato'].values[0] #type: ignore
+
+#Informações do contrato
+id_contrato = df_contratos.loc[df_contratos['contrato'] == contrato_aluno, 'id_contrato'].values[0] #type: ignore
+valor = df_contratos.loc[df_contratos['id_contrato'] == id_contrato, 'valor'].values[0] #type: ignore
+col1, col2 = st.columns(2)
+st.divider()
 with col1:
-    id_professor = selecionar_valor('Professores','name','id_professor')
+    st.subheader(contrato_aluno)
+    x_semana = df_contratos.loc[df_contratos['id_contrato'] == id_contrato, 'x_semana'].values[0] #type: ignore
+    qtd_semana = st.selectbox('Quantidade de aulas por semana', range(1,x_semana+1))
 with col2:
-    id_aluno = selecionar_valor('Alunos','name','id')
-with col3:
-    id_contrato = selecionar_valor('Contratos','contrato','id_contrato')
+    st.subheader(f'R${valor:.2f}')
+    percentual = st.selectbox('Percentual', [35,60,70,100], format_func= lambda x: f'{x}%')
+    
+valor_desconto = st.number_input('Desconto no contrato', min_value= 0, step =10)
 
-def pegar_valor(tabela_pandas, coluna_ref, coluna_buscada):
-    dataframe = pd.read_sql(f'Select * from {tabela_pandas} ',conn)
-    resultado = dataframe.loc[dataframe[coluna_ref] == coluna_ref, coluna_buscada ].values[0] #type: ignore
-    return resultado
-with col1:
-    data = st.date_input('data')
-if id_contrato:
-    #x_semana = pegar_valor('Contratos',id_contrato,'x_semana')
-    percentual = '35'
-    #valor = pegar_valor('Contratos',id_contrato,'valor')
-    desconto = 0
-    #valor_devido = valor 
-#pandas_teste = st.dataframe([selecionar_id('Professores','name','id_professor'),selecionar_id('Alunos','name','id')])
+valor_devido = ((valor-valor_desconto)/x_semana)*qtd_semana * percentual/100
+st.subheader(f'Valor devido R${valor_devido:.2f}')
 
-    frame_teste = pd.DataFrame(columns=['id_professor','id_aluno','id_contrato','x_semana','%','valor','desconto','valor_devido'])
+if st.button('Matricular'):
+    professor = F_Relatorio(db)
+    professor.insert_datas(data= data, id_professor= id_professor, id_aluno= id_aluno,id_contrato=id_contrato, x_semana= x_semana, percentual= percentual, valor= valor, valor_devido= valor_devido, desconto= valor_desconto)
+#df_frelatorio = pd.read_sql(f'Select * from F_Relatorio where id_professor = {id_professor}', conn)
 
-    professor.insert_datas(data, id_professor, id_aluno, id_contrato, x_semana, percentual, valor, desconto, valor_devido)
+st.markdown(f'{data, id_professor,  id_aluno ,id_contrato, x_semana, percentual, valor, valor_devido, valor_desconto}')
+
+st.dataframe(pd.read_sql('Select * from F_Relatorio',conn))
